83fcfe419e639d593bdaecc38d677a47a0a4e970
ahmerkhn <155139134+ahmerkhn@users.noreply.github.com>
2024-02-27 04:39:31 -0800

    ch1 created

---
+++ ch 1/app.js
@@ -0,0 +1,8 @@
+
+alert('Error! plese enter a valid password')
+
+alert('Welcome to JS land.... Happy coding!')
+
+alert("Welcome to JS land...")
+alert("Happy coading! prevent this page frome creating additiional diaglo's")
+alert("Hello i can run JS trough my web brwoser's console")
