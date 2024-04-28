8b94654e6956a742aac6d3e4bb444645d57f0a5a
ahmerkhn <155139134+ahmerkhn@users.noreply.github.com>
2024-04-28 08:33:55 -0700

    all question

--- ch 1/app.js
+++ ch 1/app.js
@@ -1,8 +1,11 @@
 
+// Q1
 alert('Error! plese enter a valid password')
 
-alert('Welcome to JS land.... Happy coding!')
+// Q2
+alert('Welcome to JS land.... \n Happy coding!')
 
+// Q3
 alert("Welcome to JS land...")
-alert("Happy coading! prevent this page frome creating additiional diaglo's")
-alert("Hello i can run JS trough my web brwoser's console")
+alert("Happy coading! \n prevent this page frome creating additiional diaglo's")
+console.log("Hello i can run JS trough my web brwoser's console")
--- ch 2/app.js
+++ ch 2/app.js
@@ -1,2 +1,12 @@
 
-alert('you age')
+// Q1
+alert('AHMER KHAN')
+alert('18')
+alert('Cirtificate Mobile Application Development')
+
+// Q2
+var name1 = ' PIZZA \n PIZZ \n PIZ \n PI'
+var email = "ahm@eg.gmail.com"
+alert(name1)
+alert(email)
+
